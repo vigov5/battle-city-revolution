41e4c53d991052b19a1cf28b279d659b2d9a9402
nguyenanhtien2210@gmail.com <nguyenanhtien2210@gmail.com@0f201495-c6b3-1e31-3394-d97205a6c412>
2011-07-05 05:43:29 +0000

    - add clock item - fix AITank

--- BattleCityRevolution/src/game/AITank.java
+++ BattleCityRevolution/src/game/AITank.java
@@ -8,53 +8,50 @@ import java.util.Random;
 public class AITank extends Tank {
 
 	private Random rnd;
-	
+
 	private boolean keepDoing = false;
-	
+
+	private boolean frezzed = false;
+
 	public AITank(BufferedImage image, int frameHeight, int frameWidth) {
 		super(image, frameHeight, frameWidth);
 		// TODO Auto-generated constructor stub
-		 rnd = new Random(new Date().getTime());
-		 this.setCurrentDirection(Sprite.DOWN);
-		 this.bulletDelayTime = 300;
-		 this.setTotalHealth(100);
-		 this.currentHealth = this.totalHealth;
-	}
-	
-	public void think(){
 		rnd = new Random(new Date().getTime());
-		int i = 0;
-		for (int j = 0; j<10; j++) i = rnd.nextInt(100);
-		
-		if (i % 37 == 0){
-			// change direction
-			this.setKeepDoing(false);
-			this.setCurrentDirection(rnd.nextInt(4));
-		}
-		
-		if (i < 50){
-			this.setKeepDoing(true);
-			if (this.pathBlocked) {
-				this.fire();
-				this.setCurrentDirection(rnd.nextInt(4)); 
-			}
-		}
-		
-		if (i % 11 == 0) fire();
+		this.setCurrentDirection(Sprite.DOWN);
+		this.bulletDelayTime = 300;
+		this.setTotalHealth(100);
+		this.currentHealth = this.totalHealth;
 	}
-	
-	public void fire(){
-		this.setRunning(false);
-		try {
-			if (System.currentTimeMillis() - lastBulletTime > bulletDelayTime) {
-				this.addBullet(this, bulletType);
-			}
-			lastBulletTime = System.currentTimeMillis();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+
+	public void think() {
+		if (frezzed){
+			this.setRunning(false);
 		}
-		this.setRunning(true);
+		else {
+			rnd = new Random(new Date().getTime());
+			int i = 0;
+			for (int j = 0; j < 10; j++)
+				i = rnd.nextInt(100);
+
+			if (i % 37 == 0) {
+				// change direction
+				this.setKeepDoing(false);
+				this.setRunning(true);
+				this.setCurrentDirection(rnd.nextInt(4));
+			}
+
+			if (i < 50) {
+				this.setRunning(true);
+				this.setKeepDoing(true);
+				if (this.pathBlocked) {
+					this.fire();
+					this.setCurrentDirection(rnd.nextInt(4));
+				}
+			}
+
+			if (i % 11 == 0)
+				fire();
+		}
 	}
 
 	public boolean isKeepDoing() {
@@ -63,5 +60,9 @@ public class AITank extends Tank {
 
 	public void setKeepDoing(boolean keepDoing) {
 		this.keepDoing = keepDoing;
+	}
+	
+	public void setFrezzed(boolean value){
+		this.frezzed = value;
 	}
 }
--- BattleCityRevolution/src/game/Item.java
+++ BattleCityRevolution/src/game/Item.java
@@ -5,7 +5,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Item extends Sprite {
-	
+
 	public final int BIG_BULLET = 0;
 	public final int MISSILE_BULLET = 1;
 	public final int LIVE = 2;
@@ -19,11 +19,11 @@ public class Item extends Sprite {
 	public Item(BufferedImage image, int frameHeight, int frameWidth) {
 		super(image, frameHeight, frameWidth);
 		// TODO Auto-generated constructor stub
-		this.setFrameStrip(new int[]{0, 1, 2, 3, 4, 5, 6});
+		this.setFrameStrip(new int[] { 0, 1, 2, 3, 4, 5, 6 });
 	}
-	
-	public void applyEffect(){
-		switch (type){
+
+	public void applyEffect() {
+		switch (type) {
 		case CLOCK:
 			break;
 		case BOMB:
@@ -33,28 +33,40 @@ public class Item extends Sprite {
 		}
 	}
 
-	public void applyEffect(Tank a){
-		switch (type){
-			case BIG_BULLET:
-			case MISSILE_BULLET:
-				a.setBulletType(type - 1);
-				break;
-			case WRECH:
-				if (a.getCurrentHealth() + 50 >=  a.totalHealth){
-					a.setCurrentHealth(a.totalHealth);
-				} else a.setCurrentHealth(a.getCurrentHealth() + 50);
-				break;
-			case THUNDER_BOOST:
-				a.setSpeedStep(4);
-				Timer t = new Timer();
-				t.schedule(new ToDoTank(this, a), 10000);
-				break;
-			case LIVE:
-				System.out.println("Add one live");
-				break;
+	public void applyEffect(Tank a) {
+		switch (type) {
+		case BIG_BULLET:
+		case MISSILE_BULLET:
+			a.setBulletType(type - 1);
+			break;
+		case WRECH:
+			if (a.getCurrentHealth() + 50 >= a.totalHealth) {
+				a.setCurrentHealth(a.totalHealth);
+			} else
+				a.setCurrentHealth(a.getCurrentHealth() + 50);
+			break;
+		case THUNDER_BOOST:
+			a.setSpeedStep(4);
+			Timer t = new Timer();
+			t.schedule(new ToDoTank(this, a), 10000);
+			break;
+		case CLOCK:
+			if (a instanceof PlayerTank) {
+				for (int i = 0; i < MainCanvas.tankArray.size(); i++) {
+					if (MainCanvas.tankArray.get(i) instanceof AITank) {
+						((AITank) MainCanvas.tankArray.get(i)).setFrezzed(true);
+					}
+				}
+			}
+			t = new Timer();
+			t.schedule(new ToDoTank(this, a), 10000);
+			break;
+		case LIVE:
+			System.out.println("Add one live");
+			break;
 		}
 	}
-	
+
 	public int getType() {
 		return type;
 	}
@@ -64,23 +76,29 @@ public class Item extends Sprite {
 		this.setFrame(type);
 	}
 
-	class ToDoTank extends TimerTask{
+	class ToDoTank extends TimerTask {
 
 		private Item item;
 		private Tank tank;
 
-		public ToDoTank(Item x, Tank y){
+		public ToDoTank(Item x, Tank y) {
 			this.item = x;
 			this.tank = y;
 		}
-		
+
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			switch (item.type){
+			switch (item.type) {
 			case THUNDER_BOOST:
 				tank.setSpeedStep(2);
-				break; 
+				break;
+			case CLOCK:
+				for (int i = 0; i < MainCanvas.tankArray.size(); i++) {
+					if (MainCanvas.tankArray.get(i) instanceof AITank) {
+						((AITank) MainCanvas.tankArray.get(i)).setFrezzed(false);
+					}
+				}
 			}
 		}
 	}
