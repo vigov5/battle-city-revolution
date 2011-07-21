fe80a40755828cb47309932b7bf99ac71f0fbb69
thangtd90@gmail.com <thangtd90@gmail.com@0f201495-c6b3-1e31-3394-d97205a6c412>
2011-07-21 08:50:46 +0000

    - Chỉnh sửa lại giao diện MAIN MENU cho phù hợp, bỏ RANKING PANEL, thêm vào đó là nút RESUME, đổi nút PLAY thành nút NEW GAME - Hoàn thành HELP PANEL và ABOUT PANEL - Cải thiện AI - Thêm các hàm kiểm tra setting (key, sound) mỗi khi resume lại game - Thêm hàm quay trở lại MAIN MENU sau khi game over
    NHỮNG VIỆC CẦN LÀM TIẾP THEO
    
    - Viết hàm restart game
    - Sửa lỗi khi pause game thì không thể thay đổi setting trong OPTION PANEL
    - Viết các hàm thay đổi máu, damage, tốc độ chạy và độ thông minh của AITank theo LEVEL
    - Cải thiện AI của máy, giúp cho AITank bắn thành khi có thế
    ......
    ........
    ..........

--- trunk/BattleCityRevolution/src/Windows/HelpPanel.java
+++ trunk/BattleCityRevolution/src/Windows/HelpPanel.java
@@ -2,6 +2,7 @@ package Windows;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Insets;
@@ -18,6 +19,12 @@ public class HelpPanel extends JPanel {
 	
 	private LinkedList listbutton = new LinkedList();
 	private Image bg;
+	String des1 = "* Use ↑ ↓ → ← and SPACE button to move UP DOWN LEFT RIGHT and FIRE. You can change those key or reset them to default at OPTION TAB.";
+	String des2 = "* At OPTION TAB you can also TURN ON or OFF the sound.";
+	String des3 = "* While playing game, use ESC button to pause game and go back to main menu. At main menu, you can choose to RESUME that game or start a NEW GAME.";
+	String des4 = "* In each game, you start with 3 lives and your mission is to protect you castle and destroy the enemies. After destroying all the enemies, you will go to the next state where the enemies become STRONGER, FASTER and SMARTER.";
+	String des5 = "* Whenever you have no more lives or the EAGLE is destroyed, THE GAME WILL BE OVER.";
+	
 	
 	public HelpPanel(){
 		setLayout(null);
@@ -36,7 +43,26 @@ public class HelpPanel extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 	    g.drawImage(bg, 100, 0, null);
+	    drawDescription(g);
 	  }
+	
+	public void drawDescription(Graphics g){
+		int i = 0;
+		g.setFont(new Font("Serif",Font.BOLD,15));
+		g.setColor(Color.BLUE);
+		g.drawString(des1.substring(0,62), 180, 120 + (i++)*20);
+		g.drawString(des1.substring(62,des1.length()), 175, 120 + (i++)*20);
+		g.drawString(des2, 180, 120 + (i++)*20);
+		g.drawString(des3.substring(0,66), 180, 120 + (i++)*20);
+		g.drawString(des3.substring(66,129), 210, 120 + (i++)*20);
+		g.drawString(des3.substring(129,des3.length()), 215, 120 + (i++)*20);
+		g.drawString(des4.substring(0,61), 220, 120 + (i++)*20);
+		g.drawString(des4.substring(61,123), 230, 120 + (i++)*20);
+		g.drawString(des4.substring(123,180), 230, 120 + (i++)*20);
+		g.drawString(des4.substring(180,des4.length()), 230, 120 + (i++)*20);
+		g.drawString(des5.substring(0,60), 250, 120 + (i++)*20);
+		g.drawString(des5.substring(60,des5.length()), 260, 120 + (i++)*20);
+	}
 	public LinkedList GetListButton(){
 		return listbutton;
 	}
